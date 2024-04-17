5abe8471c2f14e2ee8a328cfabbc98d70d314765
tiki davi <tiki davi@WIN-QPF6BK1M2AN>
2024-04-17 09:15:55 +0200

    Добавьте файлы проекта.

---
+++ WpfSqlPwz/Db.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WpfSqlPwz
+{
+    internal class Db
+    {
+        static string DBConnect = "server = localhost; user=root; password=Aa_111111; database=db";
+        
+    }
+}
---
+++ WpfSqlPwz/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSqlPwz
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //private void logIn_Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    main.Height = 360;
+        //    login.Visibility = Visibility.Visible;
+        //    pass.Visibility = Visibility.Visible;
+        //    reletePass.Visibility = Visibility.Visible;
+        //    singUp_Button.Visibility = Visibility.Hidden;
+        //    Grid.SetColumnSpan(logIn_Button, 2);
+        //    inputs.Margin = new Thickness(20, 90, 20, 0);
+        //}
+
+        //private void singUp_Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    main.Height = 360;
+        //    login.Visibility = Visibility.Visible;
+        //    pass.Visibility = Visibility.Visible;
+        //    reletePass.Visibility = Visibility.Visible;
+        //    logIn_Button.Visibility = Visibility.Hidden;
+        //    Grid.SetColumnSpan(singUp_Button, 2);
+        //    inputs.Margin = new Thickness(20, 90, 20, 0);
+
+        //}
+    }
+}
